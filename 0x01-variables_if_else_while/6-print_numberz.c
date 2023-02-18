93f5fefd8b0553b3902a702f8edec5fbb720fe1d
Ajeny090 <brianprince095@gmail.com>
2023-02-18 09:02:02 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers
+ * Return:0 (Success)
+ */
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
