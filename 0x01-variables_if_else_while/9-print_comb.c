be98e62fcd7815eb23ca556ff1a0b63f4a49dceb
Ajeny090 <brianprince095@gmail.com>
2023-02-18 10:09:08 -0800

    possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-for (j = i + 1; j <= 9; j++)
-putchar(i + '0');
+int j;
+for (j = 0; j < 10; j++)
+putchar(n);
+if (j != 10)
 putchar(',');
 putchar(' ');
-putchar(j + '0');
 putchar('\n');
 return (0);
 }
