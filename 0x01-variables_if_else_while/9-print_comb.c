d04abc2e0046926b2b00fea75c3fbc5dd23532d3
Ajeny090 <brianprince095@gmail.com>
2023-02-19 11:01:52 -0800

    last print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-/**
- * main - a program that prints single-digit numbers
+/* * main - a program that prints single-digit numbers
  * Return:0 (success)
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-j = i + '0';
-putchar(j);
-if (i != 9)
-putchar(',');
-putchar(' ');
-putchar('\n');
-return (0);
+	int i;
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		 if (i < 9)
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
